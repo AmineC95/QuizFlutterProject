2fb464bcfb2664e4bd00cd48ebbb2f58f6eb714c
AmineC95 <amine.chioukh95@gmail.com>
2024-01-12 11:05:24 +0100

    Remove unused import statements and ignore library private types in public API

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,7 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import '../models/question.dart';
-import 'dart:convert';
 
 class DatabaseManager {
   static Database? _database;
